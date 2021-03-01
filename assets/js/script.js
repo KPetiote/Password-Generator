c3fc40c8c0b9c7e8bb2329041de0d3f9b4ca1a28
Kenneth Petiote <kenneth.petiote@gmail.com>
2021-03-01 14:11:01 -0500

    Updated Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,29 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var length = 15,
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*()-=_+<>?,./";
+
+  //Ask End-User for password lenght.
+  var length = Number (prompt("How long would you like your password to be? Enter a number between 8 & 128"));
+  
+  //Ask End-User for character types to include in password.
+  var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
+
+  //Generates End-User password.
+  var charSet = "";
+  var charTypeLower = charType.toLowerCase();
+  if( charTypeLower === "lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz";
+  } else if( charTypeLower === "uppercase" ) {
+    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "numeric" ) {
+    charSet = "0123456789";
+  } else if( charTypeLower === "special" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } 
       retVal = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n));
+      for (var i = 0; i < length; i++) {
+        //Picks a character within charSet at index of random number
+        retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
   }
   return retVal;
 }
